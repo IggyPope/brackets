b8ec4e2b000373338ffb9987d0f2db587fbb9001
IggyPope <altko4egar@gmail.com>
2023-09-21 20:49:41 +0300

    feat: implement check() function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const openingBrackets = bracketsConfig.map((bracket) => bracket[0]);
+  const closingBrackets = bracketsConfig.map((bracket) => bracket[1]);
+
+  let bracketsMap = new Map();
+
+  for (let i = 0; i < openingBrackets.length; i++) {
+    bracketsMap.set(openingBrackets[i], closingBrackets[i]);
+  }
+
+  const stack = [];
+
+  const input = str.split('');
+
+  for (const bracket of input) {
+    if (openingBrackets.includes(bracket)) {
+      if (
+        closingBrackets.includes(bracket) &&
+        stack[stack.length - 1] === bracket
+      ) {
+        stack.pop();
+      } else {
+        stack.push(bracket);
+      }
+    } else if (closingBrackets.includes(bracket)) {
+      const lastBracket = stack.pop();
+
+      if (!lastBracket || bracketsMap.get(lastBracket) !== bracket) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
